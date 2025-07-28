a7498251396bdbb17195093fcaac216d7a80bc1d
K10-K10 <keito.tottori@gmail.com>
2025-07-28 23:01:44 +0900

    Create:Pass creator

--- include/create/create.cpp
+++ include/create/create.cpp
@@ -1,13 +1,80 @@
-#include "include/create/create.hpp"
-#include "include/check/check.hpp"
+#include "create/create.hpp"
+#include "check/check.hpp"
 
-std::string create_pass::pass_creator(int8_t &cmd)
+Create::Create()
 {
-	switch (cmd)
+	pass = "";
+}
+
+int Create::input_length()
+{
+	std::cout << "length:";
+	std::cin >> Create::pass_long;
+	if (Create::pass_long < 4)
+	{
+		std::cout << "Password is to short.Do you create?(y,n):";
+		char input = 'n';
+		std::cin >> input;
+		if (input != 'y' && input != 'Y')
+		{
+			std::cout << "Stop createing" << std::endl;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+std::string Create::pass_creator()
+{
+	std::cout << "Please select mode\n1 - number | 2 - number & alphabet | 3 - number,alphabet & mark | 4 - custom :";
+	std::cin >> Create::cmd;
+	switch (Create::cmd)
 	{
 	case 1:
+		if (!Create::input_length())
+		{
+			for (int i = 0; i < Create::pass_long; i++)
+			{
+				unsigned int result = Create::rng() % 10;
+				Create::pass += Create::chars[result];
+			}
+			std::cout << "Pass: " << pass << std::endl;
+		}
+		return Create::pass;
+		break;
 	case 2:
+		if (!Create::input_length())
+		{
+			for (int i = 0; i < Create::pass_long; i++)
+			{
+				unsigned int result = Create::rng() % 62;
+				Create::pass += Create::chars[result];
+			}
+			std::cout << "Pass: " << pass << std::endl;
+		}
+		return Create::pass;
+		break;
+	case 3:
+		if (!Create::input_length())
+		{
+			for (int i = 0; i < Create::pass_long; i++)
+			{
+				unsigned int result = Create::rng() % 71;
+				Create::pass += Create::chars[result];
+			}
+			std::cout << "Pass: " << pass << std::endl;
+		}
+		return Create::pass;
+		break;
+	case 4:
+		if (!Create::input_length())
+		{
+		}
+		break;
 	default:
-		std::cout << "Chose cmd" << std::endl;
-	}
+		std::cout << "Chose cmd 1 ~ 4" << std::endl;
+		return Create::pass;
+		break;
+	};
+	return 0;
 }
--- include/create/create.hpp
+++ include/create/create.hpp
@@ -1,11 +1,24 @@
+#pragma once
 #include <iostream>
 #include <random>
 #include <string>
 
-class create_pass
+class Create
 {
 public:
+	Create();
 	std::string pass;
+	std::string pass_creator();
+	int input_length();
+
+private:
+	const std::string chars =
+			"0123456789"
+			"abcdefghijklmnopqrstuvwxyz"
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			"!@#$%^&*";
+	const int zero_ascii = 48;
 	unsigned int pass_long = 0;
-	std::string pass_creator(int8_t &);
+	int cmd = 0;
+	std::random_device rng;
 };
